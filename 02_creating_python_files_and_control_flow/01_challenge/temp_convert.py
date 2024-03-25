f2d7f849e78dfdf198c986860a27e59f33ba5067
shiori-kakuchiyama <shiori.kakuchiyama@woven-planet.global>
2024-03-25 16:45:11 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,9 +1,23 @@
 def convert():
     # ここにコードを書いてください
     # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = 0
+    temp = user_input = input("温度を入力してください(fまたはcで単位を記入):")
 
+    #華氏の場合は、摂氏に変換
+    if user_input[-1] == "c" :
+        input_temp = int(user_input[:-1]) #文字列から整数に変換
+        f_temp_change = (input_temp * 9 / 5) + 32
+        f_temp = str(f_temp_change)
+        print(f_temp + "f")
+
+    elif user_input[-1] == "f":
+        input_temp = int(user_input[:-1]) #文字列から整数に変換
+        c_temp_change = 9 / 5 * (input_temp - 32)
+        c_temp = str(c_temp_change)
+        print(c_temp + "c")
+    
     return temp
+
 
 
 convert()
