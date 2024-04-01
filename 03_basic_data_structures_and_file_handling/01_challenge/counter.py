812b2d29aa4d03cc48dc691923a68d0f2265ae93
shiori-kakuchiyama <shiori.kakuchiyama@woven-planet.global>
2024-04-02 00:32:06 +0900

    Submit challenge for grading

--- 03_basic_data_structures_and_file_handling/01_challenge/counter.py
+++ 03_basic_data_structures_and_file_handling/01_challenge/counter.py
@@ -1,5 +1,5 @@
 def counter():
-    occurrences = {}
+    #occurrences = {}
     fruits = [
         "apple",
         "banana",
@@ -14,6 +14,25 @@ def counter():
     ]
 
     # ここにコードを書いてください
+    apple = ["apple" for fruits in fruits if fruits == "apple" ]
+    banana = ["banana" for fruits in fruits if fruits == "banana" ]
+    orange = ["orange" for fruits in fruits if fruits == "orange" ]
+    grape = ["grape" for fruits in fruits if fruits == "grape" ]
+    kiwi = ["kiwi" for fruits in fruits if fruits == "kiwi" ]
+    melon = ["melon" for fruits in fruits if fruits == "melon" ]
+    strawberry = ["strawberry" for fruits in fruits if fruits == "strawberry" ]
+    
+    
+    occurrences = {
+        "apple": len(apple),
+        "banana" : len(banana),
+        "orange" : len(orange),
+        "grape" : len(grape),
+        "kiwi" : len(kiwi),
+        "melon" : len(melon),
+        "strawberry" : len(strawberry)
+    }
+
 
     return occurrences
 
